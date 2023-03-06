54f8c96a8fa1a284800839344c9e5f4fad5c484c
Avi Varma <avi.varma@ymail.com>
2023-03-06 23:30:59 +0000

    Finialised libarary which populated article structure with new title and URL which is then printed into the console.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 use std::error::Error;
+use serde::Deserialize;
+use colour::{dark_green, yellow};
+
+#[derive(Deserialize, Debug)]
 
 struct Articles {
     articles: Vec<Article>
 }
 
+#[derive(Deserialize, Debug)]
 struct Article{
     title: String,
     url: String
@@ -14,12 +19,23 @@ struct Article{
 fn get_articles(url: &str) -> Result<Articles, Box<dyn Error>>{
     let response = ureq::get(url).call()?.into_string()?;
 
-    dbg!(response);
+    let articles: Articles = serde_json::from_str(&response)?;
 
-    todo!()
+    Ok(articles)
 }
 
-fn main(){
+fn render_artcles(articles: &Articles){
+    for a in &articles.articles{
+        dark_green!("> {}\n", a.title);
+        yellow!("> {}\n\n", a.url);
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>>{
     let url: &str = "https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=025d8c08ac5b4b6a910618e1b1575584";
-    let articles: Result<Articles, Box<dyn Error>> = get_articles(url);
+    let articles: Articles = get_articles(url)?;
+
+    render_artcles(&articles);
+
+    Ok(())
 }
